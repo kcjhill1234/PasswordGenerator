9114e433e89ab4e75c01bd9519a95db7cbaffb1c
ktotheH <kcjhill1234@msn.com>
2020-03-29 22:34:10 -0600

    pass

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,20 @@
 // Assignment Code
+var character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=";
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(character) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  for(var i = 0; 1<1; i++){
+   passwordText = character.charAt(Math.floor(Math.random() * character.lenght));
+  }
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+console.log(passwordText)
